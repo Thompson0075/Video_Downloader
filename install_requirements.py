043fd208d0ca9ac9cabd4302e29ffbf290d95a02
亦然Also <thompson20031207@outlook.com>
2024-12-17 19:57:38 +0800

    Update install_requirements.py

--- install_requirements.py
+++ install_requirements.py
@@ -3,14 +3,12 @@ import sys
 
 def install_requirements():
     try:
-        # 打开并读取当前目录下的 requirements.txt 文件
         with open('requirements.txt', 'r') as file:
             packages = file.readlines()
 
-        # 使用 pip 安装每个包
         for package in packages:
-            package = package.strip()  # 去除行末的换行符和多余空格
-            if package:  # 如果行不为空
+            package = package.strip() 
+            if package:
                 print(f"正在安装: {package}")
                 subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
         print("所有包安装完毕！")
